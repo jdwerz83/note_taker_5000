944f54265c1d42589874eac44e4ca58cf688eaf7
Jonathan Werz <jdwerz83@gmail.com>
2022-02-05 12:01:46 -0500

    route to notes

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 5643;
 app.use(express.static('/Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
